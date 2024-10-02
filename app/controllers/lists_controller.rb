6d0ff05b466ce7ae0f9f76b4cc0c860d0cb5e8a2
EC2 Default User <kimikuma>
2024-10-02 06:11:52 +0000

    途中

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
   def new
+    @list=List.new
   end
 
   def index
@@ -9,5 +10,17 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    list=List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+
+  private
+  #ストロングパラメーター
+  def list_params
+    params.require(:list).permit(:title,:body)
   end
 end

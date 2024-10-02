65c27a58a23c92dc5c48cc6af2163c607b196cf1
EC2 Default User <kimikuma>
2024-10-02 09:18:20 +0000

    再更新

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,15 +8,22 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list=List.find(params[:id])
   end
 
   def edit
+    @list=List.find(params[:id])
   end
+
+  def update
+    list=List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
 
   def create
     list=List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   private
